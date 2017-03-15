8d823e4475579da42d733986968f0083f150c70d
Ubuntu <ubuntu@ip-172-31-43-81.us-west-2.compute.internal>
2017-03-15 13:11:20 +0000

    path finding done. deleted async

--- euristics/ant2/main.cpp
+++ euristics/ant2/main.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
-#include <array>
-#include <async>
+#include <vector>
 #include <future>
+#include <random>
 #include <stdlib.h>
 #include <omp.h>
 #include "math.h"
@@ -19,11 +19,15 @@ class Ant {
 		Ant();
 		~Ant();
 		void reset();
-		void findpath(vector<vector<double> >);
-		void walkback(vector<vector<double> >);
+		void findpath(vector<vector<double>>);
+		void walkback(vector<vector<double>>);
 	private:
+		int pickStepLinear(int);
+		int pickStepRR(vector<vector<double>>, double, int);
+
 		vector<bool> visited;
 		vector<int> path;
+		random_device randGen;
 };
 
 typedef struct {
@@ -41,24 +45,25 @@ int startEuristic(int iterations, int colonySize) {
 	vector<Ant> ants (colonySize);
 
 	//creating the pheromone map
-	array<array<double> > pheromoneMap(numberOfLocations, vector<double>(numberOfLocations, 0.0));
+	vector<vector<double>> pheromoneMap(numberOfLocations, vector<double>(numberOfLocations, 0.0));
 
 	//starting iterations
 	for (int iter=0; iter != iterations; iter++) {
 		//reinitialize ants
-		for (auto ant : ants) ant.reset();
+		for (auto &ant : ants) ant.reset();
 
 		//ants path finding
-		vector<future<void> > tasks;
-		for (auto ant : ants) tasks.insert(async(&Ant::findpath, &ant, pheromoneMap));
-		for (auto task : tasks) task.get();
+		vector<future<void>> tasks;
+		//for (auto &ant : ants) tasks.insert(async([ant, &pheromoneMap]{ant.findpath(pheromoneMap)}));
+		//for (auto &task : tasks) task.get();
+		for (auto &ant : ants) ant.findpath(pheromoneMap);
 
 		//update pheromone
 		//each ant adds up his pheromone
-		for (auto ant : ants) ant.walkback(pheromoneMap);
+		for (auto &ant : ants) ant.walkback(pheromoneMap);
 		
 		//evaporation
-		for (auto neighbourhood : pheromoneMap)
+		for (auto &neighbourhood : pheromoneMap)
 			for (auto &edge : neighbourhood) edge = edge * (1 - EVAPORATION_RATE);
 	}
 	
@@ -80,38 +85,77 @@ Ant::Ant() {
 	visited.resize(numberOfLocations);
 	path.resize(numberOfLocations);
 	reset();
+
+	random_device randGen;
 }
 
 Ant::~Ant() {}
 
 void Ant::reset() {
-	for (auto &v : visited) v = false;
+	for (auto &&v : visited) v = false;
 	for (auto &p : path) p = -1;
 }
 
-void Ant::findpath(array<array<double> > pheromoneMap) {
+void Ant::findpath(vector<vector<double>> pheromoneMap) {
 	int current = -1;
+	int nstep = 0;
 	
 	for (auto &step : path) {
+		nstep++;
 		//always start from 0
-		if (current == 0) {
+		if (current == -1) {
 			step = 0;
 			visited[0] = true;
 			current = 0;
 			continue;
 		}
 
+		//calc total pheromone of all paths the ant can take
 		double totPheromone = 0.0;
 		for (int neighbour = 0; neighbour != numberOfLocations; neighbour++) {
-if (not visited[neighbour]) totPheromone += pheromoneMap[current][neighbour];
+			if (not visited[neighbour]) totPheromone += pheromoneMap[current][neighbour];
 		}
 		
 		//roundrobin
-		
+		//no pheromone on the road. select randomly
+		//otherwise proceed with the roundrobin random selection
+		if (totPheromone == 0) {
+			step = pickStepLinear(nstep);
+		} else {
+			step = pickStepRR(pheromoneMap, totPheromone, current);
+		}
+
+		visited[step] = true;
+		current = step;
 	}
 }
 
-void Ant::walkback(array<array<double> > pheromoneMap) {
+void Ant::walkback(vector<vector<double>> pheromoneMap) {
+}
+
+int Ant::pickStepLinear(int nstep) {
+	int stepsRemaining = numberOfLocations - nstep;
+	uniform_int_distribution<> dis(1, stepsRemaining);
+	int ran = dis(randGen);
+
+	int i;
+	for (i=0; i<=ran;) {
+		if (not visited[i]) i++;
+	}	
+
+	return i;
+}
+
+int Ant::pickStepRR(vector<vector<double>> pheromoneMap, double totPheromone, int current) {
+	uniform_real_distribution<> dis(0, totPheromone);
+	double ran = dis(randGen);
+
+	int i;
+	for (i=0; ran > 0.0; i++) {
+		if (not visited[i]) ran -= pheromoneMap[current][i];
+	}
+
+	return i;
 }
 
 //main//////////////////////////////////
