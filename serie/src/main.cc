231f954fb1331706a6de7db5e2f1e98bdc224574
Michele Proverbio <proch92@gmail.com>
2018-12-17 17:40:48 +0100

    added submodule tensorflow

---
+++ serie/src/main.cc
@@ -0,0 +1,7 @@
+#include "tensorflow/cc/client/client_session.h"
+#include "tensorflow/cc/ops/standard_ops.h"
+#include "tensorflow/core/framework/tensor.h"
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
